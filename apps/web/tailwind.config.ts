0bcd4174ac129a89e1e49fd6a64a9cf672dfd12a
Matthew Layton <layton.matt1986@gmail.com>
2024-01-29 22:51:11 -0500

    Set up global and project-specific .env files

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,10 +1,8 @@
-import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config" 
+import type { Config } from 'tailwindcss';
+import sharedConfig from '@repo/tailwind-config';
 
-const config: Pick<Config, "content" | "presets"> = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+const config: Pick<Config, 'content' | 'presets'> = {
+  content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   presets: [sharedConfig],
 };
 
--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -1,7 +1,7 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 // We want each package to be responsible for its own content.
-const config: Omit<Config, "content"> = {
+const config: Omit<Config, 'content'> = {
   theme: {
     extend: {
       // backgroundImage: {
